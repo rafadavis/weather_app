ce91302c63e03d595c276e0705750d3f4bef1ff0
Rafael Davis Portela <rdavisportela@gradcenter.cuny.edu>
2022-04-16 11:43:10 -0400

    zip code search and display working

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 const btn = document.querySelector('button');
-function test() {
 
-    const para = document.createElement('p');
-    const div = document.querySelector('div');
-    while(div.firstChild){
-        div.removeChild(div.firstChild);
-    }
-    const input = document.querySelector('input');
-    const zip = input.value;
+function drawWeather( d ) {
+    console.log(d);
+    var desc = document.getElementById('description');
+    var temp = document.getElementById('temp');
+    var loc = document.getElementById('location');
 
-    if (zip === "11377") {
-	para.textContent = "My old house";
-    } else if (zip === "11226") {
-	para.textContent = "My new place";
-    } else {
-	para.textContent = "I don't recognize this place";
-    }
-    div.appendChild(para);
+    desc.textContent += d.weather[0].description;
+    temp.textContent += d.main.temp;
+    loc.textContent += d.name;
 }
 
-btn.addEventListener('click', test);
+function request(){
+    var zipCode = document.querySelector('input').value;
+    var key = 'bc64d8909608b860393fbdaade941faf';
+    fetch('http://api.openweathermap.org/data/2.5/weather?zip='
+        + zipCode + '&appid=' + key)
+    .then(function(resp) {return resp.json() })
+    .then(function(data) {
+        drawWeather(data);
+    })
+    .catch(function() {
+    });
+}
+
+btn.addEventListener('click', function() {
+    request();
+})
 
