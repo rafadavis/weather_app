5a3acd36df6c3e92575248ce6fdf6d40f75f2a05
Rafael Davis Portela <rdavisportela@gradcenter.cuny.edu>
2022-04-17 10:43:39 -0400

    first temp check working

--- main.js
+++ main.js
@@ -6,9 +6,13 @@ function drawWeather( d ) {
     var temp = document.getElementById('temp');
     var loc = document.getElementById('location');
 
-    desc.textContent += d.weather[0].description;
-    temp.textContent += d.main.temp;
     loc.textContent += d.name;
+    cTemp = Math.round(toCelsius(d.main.temp));
+    fTemp = Math.round(toFahrenheit(d.main.temp));
+    temp.textContent += `Temperature: ${cTemp}°C /
+        ${fTemp}°F`
+    desc.textContent += d.weather[0].description[0].toUpperCase() 
+        + d.weather[0].description.substring(1); //changing case of 1st letter
 }
 
 function request(){
@@ -19,6 +23,7 @@ function request(){
     .then(function(resp) {return resp.json() })
     .then(function(data) {
         drawWeather(data);
+        drawClothes(data);
     })
     .catch(function() {
     });
@@ -27,4 +32,31 @@ function request(){
 btn.addEventListener('click', function() {
     request();
 })
+
+function toCelsius( k ) {
+    let c = k - 273.15;
+    return c
+}
+
+function toFahrenheit( k ) {
+    let f = 1.8*(k - 273) + 32;
+    return f;
+}
+
+function drawClothes( d ) {
+    var f = toFahrenheit(d.main.temp)
+    var clothes = document.getElementById("clothes");
+    if (f < 30) {
+        var dress = "dress like it's below 30"
+    } else if (f < 50) {
+        var dress = "dress like it's between 30 and 50"
+    } else if (f < 70) {
+        var dress = "dress like it's btw 50 and 70"
+    } else {
+        var dress = "more than 70"
+    }
+    console.log(dress);
+    clothes.textContent += dress;
+}
+
 
