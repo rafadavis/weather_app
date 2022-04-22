68880b71f4ea0a33c910a1f1b8cd57957a737a42
Rafael Davis Portela <rdavisportela@gradcenter.cuny.edu>
2022-04-22 09:55:18 -0400

    added event to run function with enter key press

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const key = config.SECRET_API_KEY;
 const btn = document.querySelector('button');
 btn.addEventListener('click', function() {
     request(key);
+});
+
+zipcode.addEventListener("keyup", function(event) {
+    // function to allow Enter key to trigger the function
+    if (event.keyCode === 13) {
+	request(key);
+    }
 })
 
 
